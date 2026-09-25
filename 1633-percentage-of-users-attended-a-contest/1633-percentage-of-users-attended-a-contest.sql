5ed0aaeb42463298b355b6989902c97dd6b2bf3a
AmanModi0 <amanmodi503@gmail.com>
2026-09-25 11:12:47 +0530

    Time: 909 ms (96.04%), Space: 0B (100%) - LeetHub

--- 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -1,5 +1,5 @@
 SELECT contest_id,
-       ROUND((COUNT(contest_id)/total_users)*100,2) AS percentage
-FROM Register, (SELECT COUNT(*) AS total_users FROM Users) m
+       ROUND(COUNT(contest_id)*100/(SELECT COUNT(*) FROM Users),2) AS percentage
+FROM Register
 GROUP BY contest_id
 ORDER BY percentage DESC, contest_id ASC
